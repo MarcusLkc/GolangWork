9cf8ac1170edb5d139df6b00ae3a01c96c300ee0
marcuslkc <marcuscrowder66@gmail.com>
2019-02-04 16:17:52 -0500

    wrote an interface ofr http module

--- Interfaceshttp/main.go
+++ Interfaceshttp/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	// we need to initiate byte slice because
+	// read function does not do that for us
+	// bs := make([]byte, 99999)
+	// n, err := resp.Body.Read(bs)
+	// fmt.Println(n, err)
+	// fmt.Println(string(bs))
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
